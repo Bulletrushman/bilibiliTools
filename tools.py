d254ff98a9449442fac75fdc9354890be516119c
Miles <32055007+Persistr@users.noreply.github.com>
2019-10-10 15:50:11 +0800

    增加了视频评论区锁定楼层

--- tools.py
+++ tools.py
@@ -5,6 +5,9 @@ from PIL import Image
 
 import base64
 import random
+import requests 
+import json
+import os
 
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -94,7 +97,7 @@ class LoginCollection():
         time.sleep(0.5)
         while distance > 0:
             if distance > 18:
-                span = random.randint(distance/3*2,distance-6)
+                span = random.randint(distance-12,distance-6)
             elif distance > 10:
                 # 如果距离大于10，就让他移动快一点
                 span = random.randint(5, 8)              
@@ -149,10 +152,33 @@ class LoginCollection():
         #     print("验证成功")
 
 
-# class commitCollection():
+# 抓取改视频下评论数和评论者ID   修改这里函数可以实现锁楼
+def commit(av_id):
+    r = requests.get('https://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn=1&type=1&oid={}&sort=2&_=1558948726737'.format(av_id))  
+    # 这个json地址获取方法：随便找一个评论用户的ID，在网页F12中CTRL+f 搜索 ，可以找到一个地址，复制地址，去掉callback回调部分
+    data = json.loads(r.text)
+    page_count=(data['data']['page']['count'])//20+1  # 获取总页数
+    # pprint.pprint(data['data']['replies'])
+    #这个json里不只有用户id ，而且能找到用户评论，楼中楼的用户ID 评论等
+
+    user_list=[] # 保存用户名
+
+    for pg_num in range(1,page_count+1):  # 循环获取所有页面上的用户名，这地方运行速度慢，后续尝试利用多线程加快速度
+        # print('go to {} page...'.format(pg_num))
+        r=requests.get(
+        'https://api.bilibili.com/x/v2/reply?jsonp=jsonp&pn={}&type=1&oid={}&sort=2&_=1558948726737'.format(pg_num,av_id))
+        data=json.loads(r.text)
+        
+        for i in data['data']['replies']:  # 每页有20层楼，遍历这20层楼获取ID
+            user_list.append(i['member']['uname'])
+
+    # index = 1;
+    # for i in user_list:
+    #     print(str(index) + '楼:  ' + i)
+    #     index = index + 1
+
+    print('当前评论数 ',len(user_list),'去除重复昵称后评论数 ',len(set(user_list)))
 
 
 if __name__ == '__main__':
-    login = LoginCollection()
-    login.run()
-    time.sleep(100)
+    commit(26865043)
